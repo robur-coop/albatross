18728a760141f6f67476c126b8d241124764a098
Hannes Mehnert <hannes@mehnert.org>
2020-04-03 16:34:30 +0200

    packaging/linux: cleanups of systemd scripts - console creates the run directories (albatross albatross/fifo albatross/util) -> log depends on console - albatross/fifo needs special permission (albatross_daemon creates fifo there, albatross_console needs to read them) - use /var/lib/albatross/albatross.log for the log file - only require root in albatross_daemon, use user albatross in albatross_console and albatross_log - defer albatross_stat for now

--- packaging/Linux/install.sh
+++ packaging/Linux/install.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 sudo mkdir -m 0700 -p /var/lib/albatross/block
 
-sudo cp ../../_build/default/client/*.exe /usr/local/sbin/
-sudo cp ../../_build/default/daemon/*.exe /usr/local/sbin/
-sudo cp ../../_build/default/provision/*.exe /usr/local/sbin/
-sudo cp ../../_build/default/stats/*.exe /usr/local/sbin/
-sudo cp ../../_build/default/tls/*.exe /usr/local/sbin/
-sudo cp ./albatross_*.service  /lib/systemd/system/
+sudo cp ../../_build/install/default/bin/* /usr/local/sbin/
+sudo cp ./albatross_*.service  /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl stop   albatross_console
-sudo systemctl start  albatross_console
-sudo systemctl stop  albatross_log
+sudo systemctl stop albatross_console
+sudo systemctl start albatross_console
+sudo systemctl stop albatross_log
 sudo systemctl start albatross_log
-sudo systemctl stop  albatross_stat
-sudo systemctl start albatross_stat
-sudo systemctl stop  albatross_daemon
+sudo systemctl stop albatross_daemon
 sudo systemctl start albatross_daemon
