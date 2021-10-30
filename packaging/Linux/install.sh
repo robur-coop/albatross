949c616ef41eb62b69775826e9adbbd85623c027
Hannes Mehnert <hannes@mehnert.org>
2021-10-30 18:48:17 +0200

    linux: install into /usr/sbin; systemd: expect binaries in /usr/sbin - fixes #89

--- packaging/Linux/install.sh
+++ packaging/Linux/install.sh
@@ -5,7 +5,7 @@ groupadd -g 496 $ALBATROSS_USER
 useradd -g 496 -u 496 -d /nonexistent -s /usr/sbin/nologin $ALBATROSS_USER
 sudo mkdir -m 0700 -p /var/lib/albatross/block
 
-sudo cp ../../_build/install/default/bin/* /usr/local/sbin/
+sudo cp ../../_build/install/default/bin/* /usr/sbin/
 sudo cp ./albatross_*.service ./albatross_*.socket /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl stop albatross_console
