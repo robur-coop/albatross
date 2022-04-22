7ea1e5f45890a1b418e36e15b7916d47556a04fb
Hannes Mehnert <hannes@mehnert.org>
2022-04-22 13:44:02 +0200

    bistro tests

--- test/tests.ml
+++ test/tests.ml
@@ -78,6 +78,12 @@ let name_tests = [
   "drop path is ok", `Quick, drop_path_good ;
 ]
 
+let test_version =
+  let pp_version = Vmm_commands.pp_version
+  and eq_version a b = Vmm_commands.eq_version a b
+  in
+  Alcotest.testable pp_version eq_version
+
 let test_header =
   let pp_header ppf Vmm_commands.{ version ; sequence ; name } =
     Fmt.pf ppf "version %a seq %Lu name %a"
@@ -165,11 +171,82 @@ let console_subscribe_v5_2 () =
     | _ -> Alcotest.failf "expected console_subscribe, got %a"
              (Vmm_commands.pp_wire ~verbose:true) w
 
+let to_cert s =
+  Result.get_ok (X509.Certificate.decode_pem (Cstruct.of_string s))
+
+let bistro_console_subscribe_v4 () =
+  (* albatross-client-bistro console foo --destination="-:1025" *)
+  let leaf = {|-----BEGIN CERTIFICATE-----
+MIIBbzCCASGgAwIBAgIJAOSnq8MWYF6kMAUGAytlcDASMRAwDgYDVQQDDAd0ZXN0
+LWNhMB4XDTIyMDQyMjExMDc0MloXDTIyMDQyMjExMTI1MlowDjEMMAoGA1UEAwwD
+Zm9vMCowBQYDK2VwAyEA1aQ0uXWJFl7jEYzrb6+R1IcIYxfqG8Qj38+l/kM34sSj
+gZcwgZQwGwYJKwYBBAGDhSwqBA4wDAIBBKAHoQWhAwIBFDAdBgNVHQ4EFgQU2m/k
+/dUYBNbkBn21KDhC//i8pY4wDwYDVR0PAQH/BAUDAwegADAMBgNVHRMBAf8EAjAA
+MB8GA1UdIwQYMBaAFIT1p3rnp/+V5ayQcz/BwRHYRAWeMBYGA1UdJQEB/wQMMAoG
+CCsGAQUFBwMCMAUGAytlcANBAP1eGsGgUDKDexmYYblpFvsFI5tW1RCftrnfc8JS
+KCx1B5lB+gIwj833/FzrM+RVptlvb3aIpTvscVo9fbdV+Qo=
+-----END CERTIFICATE-----|}
+  and intermediate = {|-----BEGIN CERTIFICATE-----
+MIH9MIGwoAMCAQICCBy3qcitklRSMAUGAytlcDASMRAwDgYDVQQDDAd0ZXN0LWNh
+MB4XDTIyMDQyMjExMDQ1NVoXDTMyMDQxOTExMDQ1NVowEjEQMA4GA1UEAwwHdGVz
+dC1jYTAqMAUGAytlcAMhAPsaXyTVVd4qThsmkmEdDKF2U/71RLS71Up0i9PwlSBV
+oyQwIjAPBgNVHQ8BAf8EBQMDB8YAMA8GA1UdEwEB/wQFMAMBAf8wBQYDK2VwA0EA
+33bFmS2UjE9fDZjJUgAfdZZPo1e4beaqtQ5UE2198SQfc2Xv8axiXY6R1pT5wAOm
+HUBeGB+KSdPOX8zc8taDCQ==
+-----END CERTIFICATE-----|}
+  in
+  let chain = [ to_cert leaf ; to_cert intermediate ] in
+  match Vmm_tls.handle chain with
+  | Error `Msg m -> Alcotest.failf "expected ok, got %s" m
+  | Ok (name, pols, version, command) ->
+    Alcotest.check test_name "name is foo" (n_o_s "foo") name;
+    Alcotest.check test_version "version is 4" `AV4 version;
+    Alcotest.(check bool "pols is empty" true (pols = []));
+    match command with
+    | `Console_cmd `Console_subscribe `Count 20 -> ()
+    | _ -> Alcotest.failf "expected console subscribe command, got %a"
+             (Vmm_commands.pp ~verbose:true) command
+
+let bistro_console_subscribe_v4_2 () =
+  (* albatross-client-bistro console foo.bar --destination="-:1025" *)
+  let leaf = {|-----BEGIN CERTIFICATE-----
+MIIBcjCCASSgAwIBAgIISLUDAK4z5h8wBQYDK2VwMBIxEDAOBgNVBAMMB3Rlc3Qt
+Y2EwHhcNMjIwNDIyMTEyOTU3WhcNMjIwNDIyMTEzNTA3WjASMRAwDgYDVQQDDAdm
+b28uYmFyMCowBQYDK2VwAyEAgvPFL+rH9qZTOeUQ4C2jDy5mWi1/ifPzX/cEn6ZU
+FeGjgZcwgZQwGwYJKwYBBAGDhSwqBA4wDAIBBKAHoQWhAwIBFDAdBgNVHQ4EFgQU
+//qX1UoUdCbfgI7XPInJlKrKbS4wDwYDVR0PAQH/BAUDAwegADAMBgNVHRMBAf8E
+AjAAMB8GA1UdIwQYMBaAFIT1p3rnp/+V5ayQcz/BwRHYRAWeMBYGA1UdJQEB/wQM
+MAoGCCsGAQUFBwMCMAUGAytlcANBAEFaMmq8fYFeO9CUWpa0XDo2PZ9FRqpsD70+
+UGW9OzSTXh6U+mOCJoOJMiNBOqZrgiHw4arg6LHasfprSsT+NQ0=
+-----END CERTIFICATE-----|}
+  and intermediate = {|-----BEGIN CERTIFICATE-----
+MIH9MIGwoAMCAQICCBy3qcitklRSMAUGAytlcDASMRAwDgYDVQQDDAd0ZXN0LWNh
+MB4XDTIyMDQyMjExMDQ1NVoXDTMyMDQxOTExMDQ1NVowEjEQMA4GA1UEAwwHdGVz
+dC1jYTAqMAUGAytlcAMhAPsaXyTVVd4qThsmkmEdDKF2U/71RLS71Up0i9PwlSBV
+oyQwIjAPBgNVHQ8BAf8EBQMDB8YAMA8GA1UdEwEB/wQFMAMBAf8wBQYDK2VwA0EA
+33bFmS2UjE9fDZjJUgAfdZZPo1e4beaqtQ5UE2198SQfc2Xv8axiXY6R1pT5wAOm
+HUBeGB+KSdPOX8zc8taDCQ==
+-----END CERTIFICATE-----|}
+  in
+  let chain = [ to_cert leaf ; to_cert intermediate ] in
+  match Vmm_tls.handle chain with
+  | Error `Msg m -> Alcotest.failf "expected ok, got %s" m
+  | Ok (name, pols, version, command) ->
+    Alcotest.check test_name "name is foo.bar" (n_o_s "foo.bar") name;
+    Alcotest.check test_version "version is 4" `AV4 version;
+    Alcotest.(check bool "pols is empty" true (pols = []));
+    match command with
+    | `Console_cmd `Console_subscribe `Count 20 -> ()
+    | _ -> Alcotest.failf "expected console subscribe command, got %a"
+             (Vmm_commands.pp ~verbose:true) command
+
 let command_tests = [
   "console subscribe foo version 4", `Quick, console_subscribe_v4 ;
   "console subscribe foo.bar version 4", `Quick, console_subscribe_v4_2 ;
   "console subscribe foo version 5", `Quick, console_subscribe_v5 ;
   "console subscribe foo.bar version 5", `Quick, console_subscribe_v5_2 ;
+  "bistro console subscribe foo version 4", `Quick, bistro_console_subscribe_v4 ;
+  "bistro console subscribe foo.bar version 4", `Quick, bistro_console_subscribe_v4_2 ;
 ]
 
 let tests = [
